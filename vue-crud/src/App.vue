9c2c0cdd6cb7a6ef714861739510da286b0afb3d
Polamet Wanree <Polamet.w@ku.th.com>
2024-09-15 15:02:42 +0700

    crud

---
+++ vue-crud/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+  <q-layout view="hHh lpR fFf">
+
+    <q-header elevated class="bg-primary text-white" height-hint="98">
+      <q-toolbar>
+        <q-toolbar-title>
+          <q-avatar>
+            <img src="https://cdn.quasar.dev/logo-v2/svg/logo-mono-white.svg">
+          </q-avatar>
+          Sup dude
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <q-tabs align="left">
+        <q-route-tab to="/" label="Home" />
+        <q-route-tab to="/create" label="Create" />
+        <q-route-tab to="/about" label="About" />
+      </q-tabs>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+
+  </q-layout>
+</template>
---
+++ vue-crud/src/main.js
@@ -0,0 +1,18 @@
+// import './assets/main.css'
+import { Quasar } from 'quasar'
+
+import '@quasar/extras/material-icons/material-icons.css'
+import 'quasar/src/css/index.sass'
+
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router'
+
+const app = createApp(App)
+
+app.use(router)
+app.use(Quasar,{
+  plugins: {}, // import Quasar plugins and add here
+})
+
+app.mount('#app')
